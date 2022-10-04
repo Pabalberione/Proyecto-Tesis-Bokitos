bb9472b6f040c594e2f46994b9918b639c962746
Pabalberione <pabalberione@gmail.com>
2022-10-04 08:47:42 -0300

    Comienzo eliminar proveedor

--- bokitos/applications/proveedores/urls.py
+++ bokitos/applications/proveedores/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('proveedores/', views.proveedores, name='proveedores'),
     path('proveedores/agregar_proveedor', views.agregar_proveedor, name='agregar_proveedor'),
-    path('proveedores/proveedor_agregado', views.proveedor_agregado, name='proveedor_agregado')
+    path('proveedores/proveedor_agregado', views.proveedor_agregado, name='proveedor_agregado'),
+    path('proveedores/eliminar_proveedor/<proveedor_id>', views.eliminar_proveedor, name='eliminar_proveedor'),
 
 ]
